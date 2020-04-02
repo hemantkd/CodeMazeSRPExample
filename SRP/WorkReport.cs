7ef8d8ef4498a8de3eb4153e475db85c3957c6f5
Hemant Kumar Dundigalla <d.hemant.kr@gmail.com>
2020-04-02 16:31:02 +0100

    Adding additional feature to WorkReport class. (This breaks the SRP rule as it has more than one responsibility now)

--- SRP/WorkReport.cs
+++ SRP/WorkReport.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace SRP
@@ -16,6 +17,16 @@ namespace SRP
         public void AddEntry(WorkReportEntry entry) => _entries.Add(entry);
 
         public void RemoveEntryAt(int index) => _entries.RemoveAt(index);
+
+        public void SaveToFile(string directoryPath, string filename)
+        {
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            File.WriteAllText(Path.Combine(directoryPath, filename), ToString());
+        }
 
         public override string ToString() =>
             string.Join(Environment.NewLine,
