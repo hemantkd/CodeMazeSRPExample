d49dee2815edf2209e4dd0949fa69a594b894e03
Hemant Kumar Dundigalla <d.hemant.kr@gmail.com>
2020-04-02 14:27:23 +0100

    Add project files.

---
+++ SRP/FileSaver.cs
@@ -0,0 +1,17 @@
+﻿using System.IO;
+
+namespace SRP
+{
+    public class FileSaver
+    {
+        public void SaveToFile(string directoryPath, string fileName, WorkReport report)
+        {
+            if (!Directory.Exists(directoryPath))
+            {
+                Directory.CreateDirectory(directoryPath);
+            }
+
+            File.WriteAllText(Path.Combine(directoryPath, fileName), report.ToString());
+        }
+    }
+}
